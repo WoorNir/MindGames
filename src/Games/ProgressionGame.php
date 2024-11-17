7fc9a287d025559d75d1fd3dac94cd457663fe08
Egor <woornir@yandex.ru>
2024-11-17 19:03:29 +0300

    add brain-progression game by new bin and php files

---
+++ src/Games/ProgressionGame.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Php\Project\Games\Progression;
+
+use const Php\Project\Games\Calc\GAME_DESCRIPTION as CalcGAME_DESCRIPTION;
+
+use function cli\line;
+use function cli\prompt;
+use function Php\Project\Engine\runGame;
+
+const GAME_DESCRIPTION = "What number is missing in the progression?";
+
+/**
+ * Define function to get arythmetic progression with one empty slot which is correct answer
+ */
+function getProgression($firstNumber, $difference, $length)
+{
+    $question = [];
+    for ($i = 0; $i < $length; $i++) {
+        $value = $firstNumber + $i * $difference; 
+        $question[] = $value;
+    }
+    $i = rand(1, $length - 1);
+    $correctAnswer = strval($question[$i]);
+    $question[$i] = "..";
+    $question = implode(" ", $question);
+    return [$question, $correctAnswer];
+}
+
+function playProgression()
+{
+    $callable = function() {
+        $firstNumber = rand(2, 50);
+        $difference = rand(2, 5);
+        $length = rand(5, 10);
+        [$question, $correctAnswer] = getProgression($firstNumber, $difference, $length);
+        return [$question, $correctAnswer];
+    };
+    runGame(GAME_DESCRIPTION, $callable);
+}
