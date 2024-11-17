be3a70700080bd09c15a6106719ac04c0d17ce39
Egor <woornir@yandex.ru>
2024-11-17 20:05:04 +0300

    add new game brain-prime updated Makefile with new command to launch new game

---
+++ src/Games/PrimeGame.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Php\Project\Games\Prime;
+
+use function cli\line;
+use function cli\prompt;
+use function Php\Project\Engine\runGame;
+
+const GAME_DESCRIPTION = "Answer \"yes\" if given number is prime. Otherwise answer \"no\".";
+
+/**
+ * Realize function which checking prime number
+ * and returning correct answer fo game
+ */
+function isPrime($number): string
+{
+    if ($number < 2) {
+        return 'yes';
+    }
+    for ($i = 2; $i < $number / 2; $i++) {
+        if ($number % $i === 0) {
+            return 'no';
+        }
+    }
+    return 'yes';
+}
+
+function playPrime()
+{
+    $callable = function () {
+        $question = rand(1, 100);
+        $correctAnswer = isPrime($question);
+        return [$question, $correctAnswer];
+    };
+    runGame(GAME_DESCRIPTION, $callable);
+}
